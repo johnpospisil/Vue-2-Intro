a1f73de006ad4ed293c57433494a64dcb1981441
johnpospisil <audiofreak7@gmail.com>
2019-02-28 15:47:23 -0800

    Disable re-rendering with 'v-once'

--- 02-Interact-with-the-DOM/main.js
+++ 02-Interact-with-the-DOM/main.js
@@ -1,16 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World! This is data stored in a variable in a Vue object.',
-        depart: 'Goodbye!',
+        title: 'Hello World! This is the initial sayHello() message.',
         link: 'http://google.com'
     },
     methods: {
-        sayHello: function() {
-            return 'This text is returned from the sayHello function located in the Vue object!';
-        },
-        sayGoodbye: function() {
-            return this.depart;
+        sayHello: function () {
+            this.title = "This is the updated sayHello() message.";
+            return this.title;
         }
     }
 });
