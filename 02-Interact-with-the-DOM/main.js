c35feced316e56d0d83a6a921101d3d6431751e6
johnpospisil <audiofreak7@gmail.com>
2019-02-28 15:54:38 -0800

    Output raw HTML using 'v-html'

--- 02-Interact-with-the-DOM/main.js
+++ 02-Interact-with-the-DOM/main.js
@@ -2,7 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World! This is the initial sayHello() message.',
-        link: 'http://google.com'
+        link: 'http://google.com',
+        finishedLink: '<a href="http://www.bing.com">Bing</a>'
     },
     methods: {
         sayHello: function () {
