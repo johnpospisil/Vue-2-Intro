743c2d4fb433435b7c8ec43cdec840560c83bdf6
johnpospisil <audiofreak7@gmail.com>
2019-03-04 08:21:35 -0800

    Watch Property - runls only when a property changes.

--- 02-Interact-with-the-DOM/computed-properties.js
+++ 02-Interact-with-the-DOM/computed-properties.js
@@ -4,14 +4,27 @@ new Vue({
 		counter: 0,
 		secondCounter: 0
 	},
-	// 'computed' - a new property. 
-	// more efficient than functions. These only run as needed.
+	// 'computed' properties 
+	// more efficient than functions or watch. These only run as needed.
 	// Results are cached instead of being needlessly recalculated.
+	// Runs Synchronously.
 	// Use these like a property, not like a function in your HTML file.
 	computed: {
 		output: function() {
 			console.log('Computed');
 			return this.secondCounter > 5 ? 'Greater than 5' : '5 or less'; 
+		}
+	},
+	//'watch' - a new property.
+	// Runs Asynchronously
+	// These run when a specified property changes. Below 'counter' is specified.
+	// counter is reset to '0' two seconds after the counter is first changed.
+	watch: {
+		counter: function(value) {
+			var vm = this;
+			setTimeout(function() {
+				vm.counter = 0;
+			}, 2000);
 		}
 	},
 	methods: {
